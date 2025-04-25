859cd4e399fddcdc976c94ed9441a3063659890e
dajinYang <didekwls0104@naver.com>
2025-04-25 16:51:21 +0900

    [feat] 이모지 한글자 처리

--- src/main/java/org/sopt/common/validator/TitleValidator.java
+++ src/main/java/org/sopt/common/validator/TitleValidator.java
@@ -4,14 +4,57 @@ import org.sopt.damain.api.exception.TitleEmptyException;
 import org.sopt.damain.api.exception.TitleLengthException;
 
 public class TitleValidator {
+
     public static void validateTitle(String title) {
         if (title == null || title.isBlank()) {
             throw new TitleEmptyException();
         }
 
-        if (title.length() > 30) {
+        if (countVisibleCharacters(title) > 30) {
             throw new TitleLengthException();
         }
     }
-}
 
+    private static int countVisibleCharacters(String input) {
+        int count = 0;
+        int i = 0;
+        final int len = input.length();
+
+        while (i < len) {
+            int cp = input.codePointAt(i);
+            i += Character.charCount(cp);
+
+            while (i < len) {
+                int next = input.codePointAt(i);
+                if (next == 0x200D) {
+                    i += Character.charCount(next);
+                    if (i < len) {
+                        int joined = input.codePointAt(i);
+                        i += Character.charCount(joined);
+
+                        while (i < len) {
+                            int tail = input.codePointAt(i);
+                            if (isModifier(tail) || isVariationSelector(tail)) {
+                                i += Character.charCount(tail);
+                            } else break;
+                        }
+                    } else break;
+                } else if (isModifier(next) || isVariationSelector(next)) {
+                    i += Character.charCount(next);
+                } else break;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+
+    private static boolean isModifier(int codePoint) {
+        return codePoint >= 0x1F3FB && codePoint <= 0x1F3FF;
+    }
+
+    private static boolean isVariationSelector(int codePoint) {
+        return codePoint == 0xFE0F;
+    }
+}
